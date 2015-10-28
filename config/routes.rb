403fdb912cd19575802bfd178987f53dd17e2ab5
Julien Chantereau <jchantereau@gmail.com>
2015-10-28 16:36:34 +0100

    working project without style

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
+
+  root 'restaurants#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
